1dfeff67def7dcf4adf8a682e38e9e782270a5da
Егор Меньшиков <Tyuiu.MenshikovEA@outlook.com>
2023-11-26 13:01:58 +0500

    Task5

---
+++ Tyuiu.MenshikovEA.Sprint5.Task5.V18/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.MenshikovEA.Sprint5.Task5.V18.Lib;
+
+namespace Tyuiu.MenshikovEA.Sprint5.Task5.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V18.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine("Произведение всех двузначных чисел в файле = " + res);
+
+            Console.ReadLine();
+        }
+    }
+}
